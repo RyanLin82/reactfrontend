0328f81f58b2e1a13d4a5f37025aac3bb47a085a
ryan <ryan.lin8216@gmail.com>
2019-12-23 00:52:36 +0800

    #16 getDerivedStateFromProps finish

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends React.Component {
   render(){
     return (
       <div className="App">
-        <Tier1 />
+        <Tier1 teamLead = "Jean"/>
       </div>
     );
   }
--- src/Components/Tier1.js
+++ src/Components/Tier1.js
@@ -3,16 +3,19 @@ import Tier2 from './Tier2';
 class Tier1 extends React.Component {
     constructor(props) {
         super(props);
-        console.log("[Tier1] constructor");
         this.state = {
           teamLead: "Ricky",
           show: true
         };
+        console.log("[Tier1] constructor", props, this.state);
       }
-      
+    static getDerivedStateFromProps(props, state) {
+        console.log("[Tier1 getDerivedStateFromProps]", props, state);
+        return {teamLead: props.teamLead };
+    }  
     render(){
         const { teamLead, show } = this.state;
-        console.log("[Tier1] render Tier1");
+        console.log("[Tier1] render Tier1", this.props, this.state);
         return(
             <div>
                 <header className="App-header">             
